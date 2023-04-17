9663e78c8b03209ffd61319a3bac072aa9e61ece
Sondre Lillebø Gundersen <sondrelg@live.no>
2023-04-17 23:43:10 +0200

    refactor: Split visitor logic

--- src/ast.rs
+++ src/ast.rs
@@ -3,10 +3,88 @@ use crate::gen_visitor::Visitor;
 use crate::parse_format::fix_format_call;
 use crate::parse_fstring::fix_fstring;
 use crate::{Change, SETTINGS};
-use rustpython_parser::ast::{Constant, Expr, ExprKind, Operator};
+use rustpython_parser::ast::{Constant, Expr, ExprKind, Keyword, Operator};
 
 pub(crate) struct LoggerVisitor {
     pub(crate) changes: Vec<Change>,
+}
+
+impl LoggerVisitor {
+    // f-string
+    fn handle_joinedstr(&mut self, args: &Vec<Expr>, values: &[Expr]) {
+        for expr in args {
+            let (new_string_content, new_string_variables) = fix_fstring(values);
+            if !new_string_content.is_empty() {
+                self.changes.push(Change {
+                    lineno: expr.location.row(),
+                    col_offset: expr.location.column(),
+                    end_lineno: expr.end_location.unwrap().row(),
+                    end_col_offset: expr.end_location.unwrap().column(),
+                    new_string_content,
+                    new_string_variables,
+                });
+            }
+        }
+    }
+
+    // str.format() call
+    fn handle_format_call(
+        &mut self,
+        first_value: &Expr,
+        func: &Expr,
+        args: &Vec<Expr>,
+        keywords: &Vec<Keyword>,
+    ) {
+        if let Ok(Some((new_string_content, new_string_variables))) =
+            fix_format_call(func, args, keywords)
+        {
+            self.changes.push(Change {
+                lineno: first_value.location.row(),
+                col_offset: first_value.location.column(),
+                end_lineno: first_value.end_location.unwrap().row(),
+                end_col_offset: first_value.end_location.unwrap().column(),
+                new_string_content,
+                new_string_variables,
+            });
+        }
+    }
+
+    // Might be a logger.error call
+    fn handle_call(&mut self, func: &Expr, args: &Vec<Expr>) {
+        if let ExprKind::Attribute { attr: top_attr, .. } = &func.node {
+            // First, check that the log level is valid
+            if let Some(log_level) = LogLevel::maybe_from_str(top_attr) {
+                // Next, check that the log level is above the set threshold
+                if SETTINGS.get().unwrap().log_level <= log_level {
+                    // Third, check that the first argument is an f-string or a str.format() call
+                    //
+                    // The reason for this is mainly to avoid false positives for similar syntax,
+                    // such as `messages.error(self.request, "foo")`, but it does leave us open to
+                    // false negatives from things like `logger.error("foo" + f"{bar}").
+                    // Doubt it will cause too many issues.
+                    if let Some(first_value) = args.get(0) {
+                        match &first_value.node {
+                            // f-string
+                            ExprKind::JoinedStr { values } => self.handle_joinedstr(args, values),
+                            // could be
+                            ExprKind::Call {
+                                func,
+                                args,
+                                keywords,
+                            } => {
+                                if let ExprKind::Attribute { attr, .. } = &func.node {
+                                    if attr == "format" {
+                                        self.handle_format_call(first_value, func, args, keywords);
+                                    }
+                                }
+                            }
+                            _ => (),
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 impl<'a> Visitor<'a> for LoggerVisitor {
@@ -14,106 +92,24 @@ impl<'a> Visitor<'a> for LoggerVisitor {
     /// Initially Here we're only after one type of call:
     ///
     ///    logger.info("{x}".format(x=1)
-    ///             |__ this one
+    ///            |__ this one
     ///
-    /// Info here is not actually what we're looking for; instead we'll accept any
-    /// valid python log level.
+    /// `info` here is not actually what we're looking for;
+    /// we'll accept any valid python log level.
     ///
-    /// Beyond that we have to be careful not to process syntax like
+    /// Beyond this, we have to be careful not to process syntax like
     /// `messages.error(self.request, f"{1}")` or other syntax that doesn't
-    /// exactly fit our pattern.
+    /// exactly fit our pattern. To negate this particular pattern, we've
+    /// added checking to see if the first argument to the call is a string or not.
     ///
     /// In the future, if needed, we might want to actually look for assignments from
-    /// logging.getLogger and use that when deciding which calls to handle.
+    /// logging.getLogger and use that when deciding which calls to handle, but that's
+    /// also not a fool-proof solution, as you can import loggers from other files, etc.
+    /// Loggers can also be called anything, not just `logger.info`. Many use `log.info`,
+    /// `LOG.info`, and more.
     fn visit_expr(&mut self, expr: &'a Expr) {
         match &expr.node {
-            ExprKind::Call {
-                func,
-                args,
-                keywords: _,
-            } => {
-                if let ExprKind::Attribute {
-                    value: _,
-                    attr: top_attr,
-                    ..
-                } = &func.node
-                {
-                    if let Some(log_level) = LogLevel::maybe_from_str(top_attr) {
-                        if SETTINGS.get().unwrap().log_level <= log_level {
-                            // Check that the first argument is an f-string or a str.format() call
-                            // f-strings are ExprKind::JoinedStr and str.format() calls are ExprKind::Call
-                            // The reason for this is mainly to avoid false positives for similar syntax,
-                            // such as `messages.error(self.request, "foo")`, but it does leave us open to
-                            // false negatives from things like `logger.error("foo" + f"{bar}").
-                            // Doubt it will cause too many issues.
-                            if let Some(first_value) = args.get(0) {
-                                match &first_value.node {
-                                    ExprKind::JoinedStr { values } => {
-                                        // Now that we know the first argument is a string, go ahead and process the whole call
-                                        for expr in args {
-                                            if let Some((
-                                                new_string_content,
-                                                new_string_variables,
-                                            )) = fix_fstring(values)
-                                            {
-                                                self.changes.push(Change {
-                                                    lineno: expr.location.row(),
-                                                    col_offset: expr.location.column(),
-                                                    end_lineno: expr.end_location.unwrap().row(),
-                                                    end_col_offset: expr
-                                                        .end_location
-                                                        .unwrap()
-                                                        .column(),
-                                                    new_string_content,
-                                                    new_string_variables,
-                                                });
-                                            }
-                                        }
-                                    }
-                                    ExprKind::Call {
-                                        func,
-                                        args,
-                                        keywords,
-                                    } => {
-                                        if let ExprKind::Attribute {
-                                            value: _,
-                                            attr,
-                                            ctx: _,
-                                        } = &func.node
-                                        {
-                                            if attr == "format" {
-                                                if let Some((
-                                                    new_string_content,
-                                                    new_string_variables,
-                                                )) = fix_format_call(func, args, keywords)
-                                                {
-                                                    self.changes.push(Change {
-                                                        lineno: first_value.location.row(),
-                                                        col_offset: first_value.location.column(),
-                                                        end_lineno: first_value
-                                                            .end_location
-                                                            .unwrap()
-                                                            .row(),
-                                                        end_col_offset: first_value
-                                                            .end_location
-                                                            .unwrap()
-                                                            .column(),
-                                                        new_string_content,
-                                                        new_string_variables,
-                                                    });
-                                                }
-                                            }
-                                        }
-                                    }
-                                    _ => {
-                                        // Skipping, since first argument was not an f-string or str.format call
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            ExprKind::Call { func, args, .. } => self.handle_call(func, args),
             ExprKind::BoolOp { op: _, values } => {
                 for expr in values {
                     self.visit_expr(expr);
@@ -142,7 +138,7 @@ pub fn constant_to_string(constant: Constant) -> String {
                 .map(|item| constant_to_string(item.clone()))
                 .collect::<Vec<_>>()
                 .join(", ");
-            format!("({})", items)
+            format!("({items})")
         }
     }
 }
